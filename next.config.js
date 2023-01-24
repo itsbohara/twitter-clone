d56295b5a70389c3667199dfefb013c46f1cb7ec
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-01-24 21:20:54 +0545

    api proxy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        // source: "/api/(.*)",
+        source: "/api/:path*/",
+        destination: "http://localhost:9425/api/:path*/",
+      },
+    ];
+  },
   images: {
     domains: ["pbs.twimg.com", "images.unsplash.com"],
   },

d32e34483c992d5801972f4acd8c15883c10cbce
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-01-25 13:46:46 +0545

    dynamic ui update based on profile user

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,90 +1,146 @@
-import { ReactNode } from 'react';
-import DropdownMenuDemo from '@rd/DropdownMenu';
-import HoverCardDemo from '@rd/HoverCard';
+import { ReactNode, useEffect, useState } from "react";
+import TweetDropdownMenu from "@rd/DropdownMenu";
+import HoverCardDemo from "@rd/HoverCard";
+import useAuth from "../hooks/useAuth";
+import { useAppDispatch } from "../hooks/useApp";
+import { toggleTweetLike, updateTweet } from "../redux/slices/tweet.slice";
 
 import {
-	HiOutlineHeart,
-	HiArrowUpTray,
-	HiOutlineChatBubbleOvalLeft,
-	HiOutlineArrowPath,
-	HiOutlineChartBarSquare,
-} from 'react-icons/hi2';
+  HiOutlineHeart,
+  HiHeart,
+  HiArrowUpTray,
+  HiOutlineChatBubbleOvalLeft,
+  HiOutlineArrowPath,
+  HiOutlineChartBarSquare,
+} from "react-icons/hi2";
+import axios from "@/client/axios";
+import useOnScreen from "@/hooks/useOnScreen";
+import { RiBarChartLine } from "react-icons/ri";
 
 interface Props {
-	content: string;
-	name: string;
-	username: string;
-	date: string;
-	src: string;
-	initials: string;
-	followers: string;
-	following: string;
-	description: string;
-	children?: ReactNode;
+  id: string;
+  content: string;
+  name: string;
+  username: string;
+  date: string;
+  src: string;
+  initials: string;
+  followers: string;
+  following: string;
+  description: string;
+  children?: ReactNode;
+  [key: string]: any;
 }
 
 const Post = ({
-	content,
-	name,
-	username,
-	date,
-	children,
-	src,
-	initials,
-	followers,
-	following,
-	description,
-	...props
-}: Props) => (
-	<div className="flex flex-1 gap-x-4">
-		<div className="flex-shrink-0">
-			<HoverCardDemo
-				src={src}
-				alt={name}
-				initials={initials}
-				name={name}
-				username={username}
-				following={following}
-				followers={followers}
-				description={description}
-			/>
-		</div>
-		<div className="flex flex-col flex-1">
-			<div className="flex flex-1">
-				<div className="flex flex-1 gap-x-1 text-sm">
-					<span className="text-slate-900 font-bold">{name}</span>
-					<span className="text-slate-600 font-medium">@{username}</span>·
-					<span className="text-slate-600 font-medium">{date}</span>
-				</div>
-				<div className="">
-					<DropdownMenuDemo />
-				</div>
-			</div>
-			<div className="text-sm text-slate-900 mb-4">{content}</div>
-			{children}
-			<div>
-				<ul className="flex gap-x-10 xl:gap-x-14 text-xs text-slate-700 [&_li:first-child]:hidden [&_li:first-child]:lg:flex [&_li]:flex [&_li]:items-center [&_li]:gap-x-2 [&_li:xl]:gap-x-3 ">
-					<li className="">
-						<HiOutlineChartBarSquare className="w-5 h-5" />
-						20
-					</li>
-					<li>
-						<HiOutlineChatBubbleOvalLeft className="w-5 h-5" />2
-					</li>
-					<li>
-						<HiOutlineArrowPath className="w-5 h-5" />1
-					</li>
-					<li>
-						<HiOutlineHeart className="w-5 h-5" />
-						23
-					</li>
-					<li>
-						<HiArrowUpTray className="w-5 h-5" />
-					</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-);
+  id,
+  content,
+  name,
+  username,
+  date,
+  children,
+  src,
+  initials,
+  followers,
+  following,
+  description,
+  ...props
+}: Props) => {
+  const [postVisible, postRef] = useOnScreen();
+  const { user } = useAuth();
+  const dispatch = useAppDispatch();
+  const [likedByMe, setLikedByMe] = useState(false);
+
+  const handleLikeClick = () => dispatch(toggleTweetLike(id));
+
+  useEffect(() => {
+    if (!id) return;
+    const checkLike = async () => {
+      const res = await axios.get(`/tweet/${id}/likedByMe`);
+      setLikedByMe(res.data?.liked);
+    };
+    checkLike();
+  }, []);
+  useEffect(() => {
+    if (postVisible && id) {
+      dispatch(
+        updateTweet(
+          id,
+          { viewCount: (props?.views ?? 0) + 1 },
+          { silent: true }
+        )
+      );
+    }
+  }, [postVisible]);
+
+  return (
+    <div className="flex flex-1 gap-x-4" ref={postRef}>
+      <div className="flex-shrink-0">
+        <HoverCardDemo
+          profile={src}
+          alt={name}
+          initials={initials}
+          name={name}
+          username={username}
+          following={following}
+          followers={followers}
+          description={description}
+        />
+      </div>
+      <div className="flex flex-col flex-1">
+        <div className="flex flex-1">
+          <div className="flex flex-1 gap-x-1 text-sm">
+            <span className="text-slate-900 font-bold">{name}</span>
+            <span className="text-slate-600 font-medium">@{username}</span>·
+            <span className="text-slate-600 font-medium">{date}</span>
+          </div>
+          <div className="">
+            <TweetDropdownMenu
+              username={username}
+              tweetID={id}
+              tweetByMe={user?.username === username}
+            />
+          </div>
+        </div>
+        <div className="text-sm text-slate-900">{content}</div>
+        {children}
+        <div>
+          <ul className="flex items-stretch mt-4 gap-x-10 xl:gap-x-14 text-xs text-slate-700 [&_li:first-child]:hidden [&_li:first-child]:lg:flex [&_li]:flex [&_li]:items-center [&_li]:gap-x-2 [&_li:xl]:gap-x-3 ">
+            <li>
+              <HiOutlineChartBarSquare className="w-5 h-5" />
+              {props?.views ?? 0}
+            </li>
+            <li>
+              <HiOutlineChatBubbleOvalLeft className="w-5 h-5" />
+              {props?.replyCount ?? 0}
+            </li>
+            <li>
+              <HiOutlineArrowPath className="w-5 h-5" />
+              {props?.retweetCount ?? 0}
+            </li>
+            <li onClick={handleLikeClick} className="hover:text-[#f91880]">
+              <div className="relative">
+                <div className="absolute rounded-full m-[-8px] hover:bg-[#f9188033] top-0 bottom-0 left-0 right-0"></div>
+                {likedByMe ? (
+                  <HiHeart className="w-5 h-5" />
+                ) : (
+                  <HiOutlineHeart className="w-5 h-5" />
+                )}
+              </div>
+              {props?.likes ?? 0}
+            </li>
+            <li>
+              <RiBarChartLine className="w-5 h-5" />
+              {props?.views ?? 0}
+            </li>
+            <li>
+              <HiArrowUpTray className="w-5 h-5" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Post;
--- src/components/radix/DropdownMenu.tsx
+++ src/components/radix/DropdownMenu.tsx
@@ -1,121 +1,153 @@
-import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
+import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 
-import cx from 'classnames';
-import { ReactNode } from 'react';
-import NavItem from '@ui/NavItem';
+import cx from "classnames";
+import { ReactNode } from "react";
+import NavItem from "@ui/NavItem";
 
 import {
-	HiOutlineEllipsisHorizontal,
-	HiOutlineFaceFrown,
-	HiOutlineUserPlus,
-	HiOutlineQueueList,
-	HiOutlineSpeakerXMark,
-	HiOutlineNoSymbol,
-	HiCodeBracket,
-	HiOutlineFlag,
-} from 'react-icons/hi2';
+  HiOutlineEllipsisHorizontal,
+  HiOutlineFaceFrown,
+  HiOutlineUserPlus,
+  HiOutlineQueueList,
+  HiOutlineSpeakerXMark,
+  HiOutlineNoSymbol,
+  HiCodeBracket,
+  HiOutlineFlag,
+} from "react-icons/hi2";
+import { RiDeleteBinLine } from "react-icons/ri";
+import { useAppDispatch } from "../../hooks/useApp";
+import { deleteTweet } from "@/redux/slices/tweet.slice";
 
 interface AccordionItem {
-	href: string;
-	text: string;
-	width: 'full' | 'inline' | 'mobile';
-	size: 'small' | 'default' | 'large';
-	icon?: ReactNode;
+  href: string;
+  text: string;
+  width: "full" | "inline" | "mobile";
+  size: "small" | "default" | "large";
+  icon?: ReactNode;
+  click?: void;
 }
 
-const username = 'royquilor';
+const TweetDropdownMenu = ({ username, tweetID, tweetByMe }) => {
+  const dispath = useAppDispatch();
+  const handleTweetDelete = () => dispath(deleteTweet(tweetID));
 
-const items: AccordionItem[] = [
-	{
-		href: '/',
-		text: "This Tweet's not helpful",
-		width: 'full',
-		size: 'small',
-		icon: <HiOutlineFaceFrown className="w-4 h-4" />,
-	},
-	{
-		href: '/',
-		text: `Follow @${username}`,
-		width: 'full',
-		size: 'small',
-		icon: <HiOutlineUserPlus className="w-4 h-4" />,
-	},
-	{
-		href: '/',
-		text: `Add/remove @${username} from Lists`,
-		width: 'full',
-		size: 'small',
-		icon: <HiOutlineQueueList className="w-4 h-4" />,
-	},
-	{
-		href: '/',
-		text: `Mute @${username}`,
-		width: 'full',
-		size: 'small',
-		icon: <HiOutlineSpeakerXMark className="w-4 h-4" />,
-	},
-	{
-		href: '/',
-		text: `Block @${username}`,
-		width: 'full',
-		size: 'small',
-		icon: <HiOutlineNoSymbol className="w-4 h-4" />,
-	},
-	{
-		href: '/',
-		text: 'Embed Tweet',
-		width: 'full',
-		size: 'small',
-		icon: <HiCodeBracket className="w-4 h-4" />,
-	},
-	{
-		href: '/',
-		text: 'Report Tweet',
-		width: 'full',
-		size: 'small',
-		icon: <HiOutlineFlag className="w-4 h-4" />,
-	},
-];
+  const items: AccordionItem[] | any = [
+    ...(tweetByMe
+      ? [
+          {
+            click: handleTweetDelete,
+            text: "Delete",
+            width: "full",
+            size: "small",
+            icon: <RiDeleteBinLine className="w-4 h-4" />,
+          },
+        ]
+      : [
+          {
+            href: "/",
+            text: "This Tweet's not helpful",
+            width: "full",
+            size: "small",
+            icon: <HiOutlineFaceFrown className="w-4 h-4" />,
+          },
+          {
+            href: "/",
+            text: `Follow @${username}`,
+            width: "full",
+            size: "small",
+            icon: <HiOutlineUserPlus className="w-4 h-4" />,
+          },
+          {
+            href: "/",
+            text: `Add/remove @${username} from Lists`,
+            width: "full",
+            size: "small",
+            icon: <HiOutlineQueueList className="w-4 h-4" />,
+          },
+          {
+            href: "/",
+            text: `Mute @${username}`,
+            width: "full",
+            size: "small",
+            icon: <HiOutlineSpeakerXMark className="w-4 h-4" />,
+          },
+          {
+            href: "/",
+            text: `Block @${username}`,
+            width: "full",
+            size: "small",
+            icon: <HiOutlineNoSymbol className="w-4 h-4" />,
+          },
+        ]),
 
-const DropdownMenuDemo = () => (
-	<DropdownMenuPrimitive.Root>
-		<DropdownMenuPrimitive.Trigger asChild>
-			<button
-				className="IconButton hover:bg-slate-200 rounded-full"
-				aria-label="Customize options"
-			>
-				<HiOutlineEllipsisHorizontal className="h-6 w-6" />
-			</button>
-		</DropdownMenuPrimitive.Trigger>
+    {
+      href: "/",
+      text: "Embed Tweet",
+      width: "full",
+      size: "small",
+      icon: <HiCodeBracket className="w-4 h-4" />,
+    },
+    ...(tweetByMe
+      ? []
+      : [
+          {
+            href: "/",
+            text: "Report Tweet",
+            width: "full",
+            size: "small",
+            icon: <HiOutlineFlag className="w-4 h-4" />,
+          },
+        ]),
+  ];
+  return (
+    <DropdownMenuPrimitive.Root>
+      <DropdownMenuPrimitive.Trigger asChild>
+        <button
+          className="IconButton hover:bg-slate-200 rounded-full"
+          aria-label="Customize options"
+        >
+          <HiOutlineEllipsisHorizontal className="h-6 w-6" />
+        </button>
+      </DropdownMenuPrimitive.Trigger>
 
-		<DropdownMenuPrimitive.Portal>
-			<DropdownMenuPrimitive.Content
-				sideOffset={0}
-				alignOffset={0}
-				align="end"
-				className={cx(
-					'DropdownMenuContent radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down',
-					'rounded-lg shadow-2xl w-80 overflow-hidden',
-					'bg-white border border-slate-200',
-				)}
-			>
-				{items.map(({ href, text, width, size, icon }, i) => (
-					<DropdownMenuPrimitive.Item
-						key={`header-${i}`}
-						// value={`item-${i + 1}`}
-						className="focus:outline-none overflow-hidden"
-					>
-						<NavItem href="{href}" width={width} size={size}>
-							{icon}
-							<div className="inline-flex flex-none text-lg font-medium">
-								{text}
-							</div>
-						</NavItem>
-					</DropdownMenuPrimitive.Item>
-				))}
-			</DropdownMenuPrimitive.Content>
-		</DropdownMenuPrimitive.Portal>
-	</DropdownMenuPrimitive.Root>
-);
+      <DropdownMenuPrimitive.Portal>
+        <DropdownMenuPrimitive.Content
+          sideOffset={0}
+          alignOffset={0}
+          align="end"
+          className={cx(
+            "DropdownMenuContent radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down",
+            "rounded-lg shadow-2xl w-80 overflow-hidden",
+            "bg-white border border-slate-200"
+          )}
+        >
+          {items.map(({ href, text, width, size, icon, click }, i) => (
+            <DropdownMenuPrimitive.Item
+              key={`header-${i}`}
+              // value={`item-${i + 1}`}
+              className="focus:outline-none overflow-hidden"
+            >
+              {click ? (
+                <NavItem button onClick={click} width={width} size={size}>
+                  {icon}
+                  <div className="inline-flex flex-none text-lg font-medium">
+                    {text}
+                  </div>
+                </NavItem>
+              ) : (
+                <NavItem href={href} width={width} size={size}>
+                  {icon}
+                  <div className="inline-flex flex-none text-lg font-medium">
+                    {text}
+                  </div>
+                </NavItem>
+              )}
+            </DropdownMenuPrimitive.Item>
+          ))}
+        </DropdownMenuPrimitive.Content>
+      </DropdownMenuPrimitive.Portal>
+    </DropdownMenuPrimitive.Root>
+  );
+};
 
-export default DropdownMenuDemo;
+export default TweetDropdownMenu;

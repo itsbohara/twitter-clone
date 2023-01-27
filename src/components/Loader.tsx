562ebaf0f4a12b102002dc1191a1daf114915a2f
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-01-27 21:58:46 +0545

    ui : loading indicator update

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -4,7 +4,7 @@ export default function Loader() {
       <div role="status">
         <svg
           aria-hidden="true"
-          className="w-5 h-5 mr-2 animate-spin text-blue-200  fill-blue-600"
+          className="w-5 h-5 mr-2 animate-spin text-blue-200  fill-blue-400"
           viewBox="0 0 100 101"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"

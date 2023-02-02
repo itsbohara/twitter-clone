9db804d1d10109db97ddec04d50c6e3fb1449830
Mahesh Bohara <13344241+itsbohara@users.noreply.github.com>
2023-02-02 07:56:45 +0545

    ✅ user profile hover blue badge

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,3 +1,6 @@
+import TwitterBlueCheck from "./TwitterBlueCheck";
+import { AccountSubscription } from "../types/user";
+import Link from "next/link";
 const UserCard = ({
   name,
   username,
@@ -6,29 +9,39 @@ const UserCard = ({
   followers,
   subscription,
 }: {
-	name: string;
-	username: string;
-	description?: string;
-	following?: string;
-	followers?: string;
-}) => (
-	<>
-		<div className="mb-2">
-			<div className="text-base font-semibold leading-none">{name}</div>
-			<div className="text-sm text-slate-500 font-medium">@{username}</div>
-		</div>
-		<div className="text-sm mb-2">{description}</div>
-		<div className="flex gap-x-4">
-			<div className="flex gap-x-1">
-				<div className="text-sm font-bold leading-none">{following ?? 0}</div>
-				<div className="text-sm text-slate-500 leading-none">Following</div>
-			</div>
-			<div className="flex gap-x-1">
-				<div className="text-sm font-bold leading-none">{followers ?? 0}</div>
-				<div className="text-sm text-slate-500 leading-none">Followers</div>
-			</div>
-		</div>
-	</>
-);
+  name: string;
+  username: string;
+  bio?: string;
+  following?: number;
+  followers?: number;
+  subscription?: AccountSubscription;
+}) => {
+  const bioAvailable = bio && bio?.trim() !== "";
+  return (
+    <>
+      <div className="my-2">
+        <Link
+          href={`/${username}`}
+          className="flex items-center text-base font-semibold leading-none hover:underline"
+        >
+          <span>{name}</span>
+          <TwitterBlueCheck subscription={subscription} />
+        </Link>
+        <div className="text-sm text-slate-500 font-medium">@{username}</div>
+      </div>
+      {bioAvailable && <div className="text-sm mb-2">{bio}</div>}
+      <div className="flex gap-x-4">
+        <div className="flex gap-x-1">
+          <div className="text-sm font-bold leading-none">{following ?? 0}</div>
+          <div className="text-sm text-slate-500 leading-none">Following</div>
+        </div>
+        <div className="flex gap-x-1">
+          <div className="text-sm font-bold leading-none">{followers ?? 0}</div>
+          <div className="text-sm text-slate-500 leading-none">Followers</div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default UserCard;
